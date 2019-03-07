48597ff9eddb9c36da8881fa2afdb448cb814b68
Gabriel <gdeperes@gmail.com>
2019-03-07 03:57:26 -0300

    Fixed re-initializing submodules when they were are already initialized

--- Findadvencore.cmake
+++ Findadvencore.cmake
@@ -1,14 +1,17 @@
-find_path(ADVENCORE_INCLUDE_DIR tonc.h)
-find_library(ADVENCORE_LIBRARY NAMES libtonc.a)
+find_path(ADVENCORE_INCLUDE_DIR advenCore/VBAPrint.hpp)
+find_library(ADVENCORE_LIBRARY NAMES advencore.a)
 
 if (ADVENCORE_INCLUDE_DIR AND ADVENCORE_LIBRARY)
     add_library(advencore INTERFACE)
-    target_include_directories(advencore INTERFACE ${TONC_INCLUDE_DIR})
-    target_link_libraries(advencore INTERFACE ${TONC_LIBRARY})
+    target_include_directories(advencore INTERFACE ${ADVENCORE_INCLUDE_DIR})
+    target_link_libraries(advencore INTERFACE ${ADVENCORE_LIBRARY})
 else()
     message("Couldn't find advencore, downloading from GitHub")
-    execute_process(COMMAND git submodule update --init -- external/AdvenCore
-                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+    if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/external/AdvenCore/.git)
+        execute_process(COMMAND git submodule update --init -- external/AdvenCore
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    endif()
     
     add_subdirectory(external/AdvenCore)
 endif()
--- Findlibgbfs.cmake
+++ Findlibgbfs.cmake
@@ -1,14 +1,17 @@
-find_path(LIGBGBFS_INCLUDE_DIR tonc.h)
-find_library(LIBGBFS_LIBRARY NAMES libtonc.a)
+find_path(LIGBGBFS_INCLUDE_DIR gbfs.h)
+find_library(LIBGBFS_LIBRARY NAMES libgbfs.a)
 
 if (LIBGBFS_INCLUDE_DIR AND LIBGBFS_LIBRARY)
     add_library(libgbfs INTERFACE)
-    target_include_directories(libgbfs INTERFACE ${TONC_INCLUDE_DIR})
-    target_link_libraries(libgbfs INTERFACE ${TONC_LIBRARY})
+    target_include_directories(libgbfs INTERFACE ${LIBGBFS_INCLUDE_DIR})
+    target_link_libraries(libgbfs INTERFACE ${LIBGBFS_LIBRARY})
 else()
     message("Couldn't find libgbfs, downloading from GitHub")
-    execute_process(COMMAND git submodule update --init -- external/libgbfs
-                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    
+    if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/external/libgbfs/.git)
+        execute_process(COMMAND git submodule update --init -- external/libgbfs
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    endif()
     
     add_subdirectory(external/libgbfs)
 endif()
